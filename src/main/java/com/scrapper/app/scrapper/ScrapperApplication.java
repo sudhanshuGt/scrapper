179eb3c552d2625086425bd0eb7fa11a600065cf
sudhanshuGt <sudhanshutiwari357@gmail.com>
2023-03-07 14:43:27 +0530

    initial commit

---
+++ src/main/java/com/scrapper/app/scrapper/ScrapperApplication.java
@@ -0,0 +1,13 @@
+package com.scrapper.app.scrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapperApplication.class, args);
+	}
+
+}
